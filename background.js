43d009ebc756bcd16531b38315a90dd7a60d99ef
hzm <467251648@qq.com>
2025-04-17 23:39:11 +0800

    脚本弹窗添加读取local数据

--- background.js
+++ background.js
@@ -1,71 +1,63 @@
 // 主要与chrome进行后台交互
 
 chrome.runtime.onInstalled.addListener(async () => {
-	console.log("background ====== plugin_installed");
+  console.log("background ====== plugin_installed");
 });
 
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-	console.log("background ==== receive", message, sender, sendResponse);
-	if (message.action == "getToken") {
-		getToken(message.data);
-	} else if (message.action == "uploadFile") {
-		uploadSyncFile(message.data);
-	} else if (message.action == "getAppInfo") {
-		console.log("1");
-		const data = await chrome.storage.sync.get("appInfo");
-		console.log("2");
-		sendResponse("123");
-		console.log("3");
-	} else if (message.action == "setAppInfo") {
-		chrome.storage.sync.set("appInfo", message.data);
-	}
-	return true;
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  console.log("background ==== receive");
+  switch (request.action) {
+    case "getToken":
+      getToken(request.data);
+      break;
+    case "uploadFile":
+      uploadSyncFile(request.data);
+      break;
+    default:
+      break;
+  }
+  return true; //开启异步
 });
-
-async function getApp() {
-	const data = await chrome.storage.local.get("appInfo");
-	return "2134";
-}
 
 // 飞书的请求放在 content-script.js 中会显示跨域
 async function getToken(params) {
-	try {
-		const existValue = await chrome.storage.local.get(); // 传不传key都一样
-		console.log("---开放token 开始");
-		const backData = await fetch(
-			"https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal",
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json", // 设置请求头（根据实际需求调整）
-				},
-				body: JSON.stringify(params),
-			}
-		);
-		const tempAuth = await backData.json();
-		if (tempAuth?.tenant_access_token) {
-			chrome.storage.local.set({
-				accessToken: tempAuth.tenant_access_token,
-			});
-			console.log("---开放token 成功");
-		}
-		return tempAuth.tenant_access_token;
-	} catch (error) {
-		console.error("---开放token 失败：", error);
-	}
+  try {
+    const existValue = await chrome.storage.local.get(); // 传不传key都一样
+    console.log("---开放token 开始");
+    const backData = await fetch(
+      "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // 设置请求头（根据实际需求调整）
+        },
+        body: JSON.stringify(params),
+      }
+    );
+    const tempAuth = await backData.json();
+    if (tempAuth?.tenant_access_token) {
+      chrome.storage.local.set({
+        accessToken: tempAuth.tenant_access_token,
+      });
+      console.log("---开放token 成功");
+    }
+    return tempAuth.tenant_access_token;
+  } catch (error) {
+    console.error("---开放token 失败：", error);
+  }
 }
 
 async function uploadSyncFile(records) {
-	const existValue = await chrome.storage.local.get();
-	const data = await fetch(
-		`https://open.feishu.cn/open-apis/bitable/v1/apps/RFwHb89ztawvSusGzTzcDHKdnid/tables/tblowQSvTIZI5i5g/records/batch_create`,
-		{
-			method: "POST", // 指定方法为 POST
-			headers: {
-				Authorization: `Bearer ${existValue.accessToken}`,
-			},
-			body: JSON.stringify({ records }),
-		}
-	);
-	console.log("upload =====", data);
+  const existValue = await chrome.storage.local.get();
+  const data = await fetch(
+    `https://open.feishu.cn/open-apis/bitable/v1/apps/RFwHb89ztawvSusGzTzcDHKdnid/tables/tblowQSvTIZI5i5g/records/batch_create`,
+    {
+      method: "POST", // 指定方法为 POST
+      headers: {
+        Authorization: `Bearer ${existValue.accessToken}`,
+      },
+      body: JSON.stringify({ records }),
+    }
+  );
+  console.log("upload =====", data);
 }
--- popup/index.js
+++ popup/index.js
@@ -1,110 +1,104 @@
 console.log("popup ====== start");
 
 const appInfo = {
-	appId: "",
-	appSecretId: "",
-	tableUrl: "",
+  appId: "",
+  appSecretId: "",
+  tableUrlId: "",
 };
 
 function setIninValue(targetId, value) {
-	document.getElementById(targetId).value = value;
+  document.getElementById(targetId).value = value;
 }
+
+// 读取缓存信息
+chrome.storage.local.get("appInfo", (result) => {
+	if (chrome.runtime.lastError) {
+	  console.log("读取失败", chrome.runtime.lastError);
+	} else {
+	  for (let i in appInfo) {
+		appInfo[i] = result.appInfo[i];
+		setIninValue(i, result.appInfo[i]);
+	  }
+	}
+  });
+  
 
 function addListen(targetId, callBack = null, type = "click") {
-	if (!document) {
-		return;
-	}
-	switch (type) {
-		case "change":
-			document.getElementById(targetId).addEventListener("change", () => {
-				callBack && callBack();
-			});
-			break;
-		default: // click
-			document.getElementById(targetId).addEventListener("click", () => {
-				callBack && callBack();
-			});
-			break;
-	}
+  if (!document) {
+    return;
+  }
+  switch (type) {
+    case "change":
+      document.getElementById(targetId).addEventListener("change", (e) => {
+        callBack && callBack(e.target.value);
+      });
+      break;
+    default: // click
+      document.getElementById(targetId).addEventListener("click", (e) => {
+        callBack && callBack(e);
+      });
+      break;
+  }
 }
 
-chrome.runtime.sendMessage(
-	{
-		action: "getAppInfo",
-	},
-	(data) => {
-		console.log("data134", data);
-	}
-);
-
-chrome.runtime.onMessage.addListener(async (data) => {
-	console.log("popup ==== receive", data);
-	if (data.action == "readAppInfo") {
-		for (let i in appInfo) {
-			appInfo[i] = data.data[i];
-			setIninValue(i, data.data[i]);
-		}
-		appInfo = data.data;
-	}
-});
 
 addListen("exportFeiSu", handleFeisu);
 
 addListen("exportCsv", handleCsv);
 
-function handleChange(key, value) {
-	console.log("value", key, value);
-	chrome.runtime.sendMessage({
-		id: "scrapePlugin",
-		from: "popup",
-		action: "saveAppInfo",
-		data: {
-			...appInfo,
-			[key]: value,
-		},
-	});
-}
-
-["appId", "appSecretId", "tableUrlId"].map((it) =>
-	addListen(it, (e) => handleChange(it, e), "change")
-);
-
 function handleCsv(data) {
-	let csv = "\n";
-	// csv += `"${data.pageTitle}","${data.pageUrl}"\n\n`;
+  let csv = "\n";
+  // csv += `"${data.pageTitle}","${data.pageUrl}"\n\n`;
 
-	// 表头
-	csv += `\n${data.tableHeads.join(",")}\n`;
-	// 表内
-	data.tableList.forEach((l) => (csv += `${l.join(",")}\n`));
+  // 表头
+  csv += `\n${data.tableHeads.join(",")}\n`;
+  // 表内
+  data.tableList.forEach((l) => (csv += `${l.join(",")}\n`));
 
-	// 触发下载
-	const blob = new Blob(["\uFEFF" + csv], { type: "text/csv;charset=utf-8;" });
-	const url = URL.createObjectURL(blob);
-	const a = document.createElement("a");
-	a.href = url;
-	a.download = `商品${new Date().toLocaleString()}.csv`;
-	a.click();
+  // 触发下载
+  const blob = new Blob(["\uFEFF" + csv], { type: "text/csv;charset=utf-8;" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `商品${new Date().toLocaleString()}.csv`;
+  a.click();
 }
 
 function handleFeisu() {
-	console.log("点击", document.getElementById("fileInput"));
-	chrome.runtime.sendMessage({
-		id: "scrapePlugin",
-		action: "uploadFile",
-		records: [
-			{
-				fields: {
-					名称: "茶叶",
-					详情: "https://juejin.cn/post/7485631488115228726",
-				},
-			},
-			{
-				fields: {
-					名称: "茶叶1",
-					详情: "https://juejin.cn/post/7485631488115228726",
-				},
-			},
-		],
-	});
+  console.log("点击", document.getElementById("fileInput"));
+  chrome.runtime.sendMessage({
+    id: "scrapePlugin",
+    action: "uploadFile",
+    records: [
+      {
+        fields: {
+          名称: "茶叶",
+          详情: "https://juejin.cn/post/7485631488115228726",
+        },
+      },
+      {
+        fields: {
+          名称: "茶叶1",
+          详情: "https://juejin.cn/post/7485631488115228726",
+        },
+      },
+    ],
+  });
 }
+
+function handleChange(key, value) {
+	appInfo[key] = value;
+	chrome.storage.local.set(
+	  {
+		appInfo,
+	  },
+	  () => {
+		console.log("appInfo 存储数据");
+	  }
+	);
+  }
+  
+  ["appId", "appSecretId", "tableUrlId"].map((it) =>
+	addListen(it, (e) => handleChange(it, e), "change")
+  );
+  
